92ab26bc1cf04e09576c15f248ced193f1e61318
Leandro Carrizo <leandropuntomp3@gmail.com>
2023-04-30 01:03:46 -0300

    deploy

--- componets/Navbar.js
+++ componets/Navbar.js
@@ -1,7 +1,7 @@
 import Link from "next/link"
 
 const Navbar = () => (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <nav className="navbar navbar-expand navbar-light bg-light fixed-top">
         <div className="container ">
             <Link className="navbar-brand" href="/">Home</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
