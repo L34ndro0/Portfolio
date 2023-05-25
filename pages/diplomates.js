8741aff508d7cb4294b2c0230dcdee1ad10b06e3
Leandro Carrizo <leandropuntomp3@gmail.com>
2023-05-25 18:17:30 -0300

    deploy vercel api

--- pages/diplomates.js
+++ pages/diplomates.js
@@ -1,5 +1,5 @@
 import Layout from "../componets/Layout"
-import PDFviewer from "../componets/PDFviewer"
+import PDFviewer from '../componets/PDFviewer'
 import { useState, useEffect } from "react";
 
 const Diplomates = () => {
