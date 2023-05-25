e2bb755bb5df5c193e58ec4d53d617a269a42249
Leandro Carrizo <leandropuntomp3@gmail.com>
2023-05-25 18:11:01 -0300

    deploy vercel api

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d44a5c841e468de3"; }
+/******/ 		__webpack_require__.h = function() { return "a4e47438693480af"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
